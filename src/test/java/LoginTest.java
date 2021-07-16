0f72c7933f12723a1ce6e210b73faef70f325b96
LaraLark <86426630+LaraLark@users.noreply.github.com>
2021-07-16 16:45:12 +0300

    Added UserHelper

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,41 @@
+import models.User;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTest extends TestBase{
+
+    @BeforeMethod
+    public void precondition(){
+        if(!app.userHelper().isLogged()){
+            app.userHelper().logout();
+        }
+    }
+    @Test
+    public void loginTestPositive(){
+
+        app.userHelper().openLoginForm();
+        app.userHelper().fillLoginForm("noa@gmail.com","Nnoa12345$");
+        app.userHelper().submitLogin();
+        app.userHelper().pause(3000);
+        String loginS=app.userHelper().getText(By.xpath("//div[@class='dialog-container']//h2"));
+        app.userHelper().clickOkButton();
+        Assert.assertEquals(loginS,"Logged in success");
+
+    }
+    @Test
+    public void loginTestPositiveDto(){
+        User user=new User().withEmail("noa@gmail.com").whithPassword("Nnoa12345$");
+
+        app.userHelper().openLoginForm();
+        app.userHelper().fillLoginForm(user);
+        app.userHelper().submitLogin();
+        app.userHelper().pause(3000);
+        String loginS=app.userHelper().getText(By.xpath("//div[@class='dialog-container']//h2"));
+        app.userHelper().clickOkButton();
+        Assert.assertEquals(loginS,"Logged in success");
+
+    }
+}
